1b22e82af54da8be4ca359c343a224d5af650f90
andrewjuarez <a777juarez@gmail.com>
2022-01-24 15:29:39 -0800

    [DEV] Set up mock data files.

---
+++ api/mockData/filmSearch.mockdata.js
@@ -0,0 +1,41 @@
+const mockData = {
+    data: {
+        status: 'OK',
+        status_message: "Query was successful",
+        search_results: 6,
+        movie_results: [
+            {
+                "imdb_id": "tt1974203",
+                "title": "Adventures in Odyssey: Escape from the Forbidden Matrix",
+                "year": 2001
+            },
+            {
+                "imdb_id": "tt0109151",
+                "title": "Armitage III: Poly Matrix",
+                "year": 1996
+            },
+            {
+                "imdb_id": "tt0303678",
+                "title": "Armitage: Dual Matrix",
+                "year": 2002
+            },
+            {
+                "imdb_id": "tt4074340",
+                "title": "Elektro Mathematrix",
+                "year": 2016
+            },
+            {
+                "imdb_id": "tt1595473",
+                "title": "The American Matrix - Age Of Deception",
+                "year": 2010
+            },
+            {
+                "imdb_id": "tt0328832",
+                "title": "The Animatrix",
+                "year": 2003
+            }
+        ]
+    }
+}
+
+exports.mockData = mockData;
