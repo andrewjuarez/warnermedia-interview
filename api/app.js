fb9cd1e776d72c148b846961e6abe064edc91e73
andrewjuarez <a777juarez@gmail.com>
2022-01-24 14:32:46 -0800

    [DEV] Enabled CORS on api server.

--- api/app.js
+++ api/app.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 var morgan = require('morgan');
 app.use(morgan('dev'));
-
+var cors = require('cors');
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200
+  }));
+  
 const router = require('./routes');
 app.use(router);
 
