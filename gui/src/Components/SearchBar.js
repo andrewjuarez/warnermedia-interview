a75a2831c98c73527c24da5ea0b15bedf58c1936
andrewjuarez <a777juarez@gmail.com>
2022-01-24 14:35:27 -0800

    [DEV] Display search results underneath search bar.

--- gui/src/Components/SearchBar.js
+++ gui/src/Components/SearchBar.js
@@ -1,12 +1,40 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { searchMovies } from '../Actions/search.actions';
 import {
-    Button,
     Form,
     FormControl,
+    Dropdown,
+    DropdownButton
 } from 'react-bootstrap';
 
 export default function SearchBar() {
-    const [query, setQuery] = useState('');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [query, setQuery] = useState(''); // The query present in search bar
+    const [changeDetected, setChangeDetected] = useState(false); // Track if change is detected to prevent duplicate responses to DB
+    const { loading, results, success } = useSelector(state => state.search);
+
+    /**
+     * @description - Update the query and variable tracking change.
+     * @param value {string} - New value entered by user.
+     */
+    const updateQuery = (value) => {
+        setChangeDetected(true);
+        setQuery(value)
+    }
+
+    /**
+     * @description - Check if new query is detected, fetch results from API, if not display previous search results
+     */
+    const searchNow = () => {
+        if (changeDetected) {
+            setChangeDetected(false);
+            dispatch(searchMovies(query));
+        }
+    }
+
     return (
         <Form className='d-flex'>
             <FormControl
@@ -15,9 +43,13 @@ export default function SearchBar() {
                 className='me-2'
                 aria-label='Search'
                 value={query}
-                onChange={(e) => setQuery(e.target.value)}
+                onChange={(e) => updateQuery(e.target.value)}
             />
-            <Button onClick={() => console.log(query)} variant='outline-success'>Search</Button>
+            <DropdownButton onClick={searchNow} onSelect={(eventKey) => navigate(`/details/${eventKey}`)} align="end" title="Search" id="dropdown-menu-align-end">
+                {results.map(result => (
+                    <Dropdown.Item key={result.imdb_id} eventKey={result.imdb_id}>{result.year} | {result.title}</Dropdown.Item>
+                ))}
+            </DropdownButton>
         </Form>
     );
 }
--- gui/src/Reducers/root.reducer.js
+++ gui/src/Reducers/root.reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import searchReducer from './search.reducer';
+import movieDetailsReducer from './movieDetails.reducer';
 
 const rootReducer = combineReducers({
     search: searchReducer,
+    movieDetails: movieDetailsReducer
 });
 
 export default rootReducer;
