79f5455a6b1364e16dd56fe4893ea1b50b8175eb
andrewjuarez <a777juarez@gmail.com>
2022-01-24 20:15:22 -0800

    [DEV] Finished details page.

--- gui/src/Pages/Details.js
+++ gui/src/Pages/Details.js
@@ -15,45 +15,56 @@ export default function DetailsPage() {
 
     useEffect(() => {
         dispatch(getMovieDetails(params.movieId));
-    }, [])
+    }, [params.movieId])
 
-    console.log('details: ', details)
-    return (
-        <div>
-            <h1>{details.title}</h1>
+    if (success) {
+        return (
             <div>
-                {details.year} | {details.rated} | {Math.floor(details.runtime / 60)}h {details.runtime % 60}m
-            </div>
-            <YouTubeFrame embedId={details.youtube_trailer_key} />
-            <div>
-                {details.genres.map((genre, index) => {
-                    return <Badge key={index} pill bg="warning" text="dark">{genre}</Badge>
-                })}
-            </div>
-            <p>{details.description}</p>
+                <h1>{details.title}</h1>
+                <div>
+                    {details.year} | {details.rated} | {Math.floor(details.runtime / 60)}h {details.runtime % 60}m
+                </div>
+                <YouTubeFrame embedId={details.youtube_trailer_key} />
+                <div>
+                    {details.genres.map((genre, index) => {
+                        return <Badge key={index} pill bg="warning" text="dark">{genre}</Badge>
+                    })}
+                </div>
+                <p>{details.description}</p>
 
-            <div>⭐ {details.imdb_rating}/10</div>
+                <div>⭐ {details.imdb_rating}/10</div>
 
-            <Accordion defaultActiveKey="0">
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>Directors</Accordion.Header>
-                    <Accordion.Body>
-                        {details.directors.map(director => (director))}
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="1">
-                    <Accordion.Header>Cast</Accordion.Header>
-                    <Accordion.Body>
-                        {details.stars.map(star => (star))}
-                    </Accordion.Body>
-                </Accordion.Item>
-                <Accordion.Item eventKey="3">
-                    <Accordion.Header>Globalization</Accordion.Header>
-                    <Accordion.Body>
-                        {details.countries.map(country => (country))}
-                    </Accordion.Body>
-                </Accordion.Item>
-            </Accordion>
-        </div>
-    );
+                <Accordion defaultActiveKey="0">
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>Directors</Accordion.Header>
+                        <Accordion.Body>
+                            <ul>
+                                {details.directors.map(director => (<li>{director}</li>))}
+                            </ul>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>Cast</Accordion.Header>
+                        <Accordion.Body>
+                            <ul>
+                                {details.stars.map(star => (<li>{star}</li>))}
+                            </ul>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                    <Accordion.Item eventKey="2">
+                        <Accordion.Header>Globalization</Accordion.Header>
+                        <Accordion.Body>
+                            <ul>
+                                {details.countries.map(country => (<li>{country}</li>))}
+                            </ul>
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </div>
+        );
+    } else if(loading) {
+        return <p>Loading</p>
+    } else {
+        return <p>Error</p>
+    }
 }
