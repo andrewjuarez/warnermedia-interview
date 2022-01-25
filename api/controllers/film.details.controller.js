9dc6272983e9de82f7e8de2e6bd338dbc55b0176
andrewjuarez <a777juarez@gmail.com>
2022-01-24 20:16:18 -0800

    [DEV] Enabled API controllers.

--- api/controllers/film.details.controller.js
+++ api/controllers/film.details.controller.js
@@ -1,3 +1,19 @@
+const axios = require('../axios');
+const { mockData } = require('../mockData/filmDetails.mockdata');
+
 module.exports = async function filmDetailsController(req, res) {
-    res.status(200).send({"message":"Hello world!"})
+    try {
+        var apiResponse = await axios.get('https://movies-tvshows-data-imdb.p.rapidapi.com/',
+            { params: { type: 'get-movie-details', imdb: req.params.movieId } });
+        // var apiResponse = mockData;
+        // console.log(apiResponse)
+        if (apiResponse?.data?.status === 'OK') {
+            res.status(200).send({ success: true, details: apiResponse.data });
+        } else {
+            throw new Error("API error");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, message: 'Error' });
+    }
 }
--- api/controllers/film.search.controller.js
+++ api/controllers/film.search.controller.js
@@ -3,16 +3,17 @@ const { mockData } = require('../mockData/filmSearch.mockdata');
 
 module.exports = async function filmSearchController(req, res) {
     try {
-        // var apiResponse = await axios.get('https://movies-tvshows-data-imdb.p.rapidapi.com/',
-        //     { params: { type: 'get-movies-by-title', title: req.query.title } });
-        var apiResponse = mockData;
-        console.log(apiResponse.data)
+        var apiResponse = await axios.get('https://movies-tvshows-data-imdb.p.rapidapi.com/',
+            { params: { type: 'get-movies-by-title', title: req.query.title } });
+        // var apiResponse = mockData;
+        // console.log(apiResponse.data)
         if (apiResponse?.data?.status === 'OK') {
             res.status(200).send({ success: true, movies: apiResponse.data.movie_results });
         } else {
             throw new Error("API error");
         }
     } catch (error) {
+        console.error(error);
         if (error?.response?.status === 403) {
             res.status(500).send({ success: false, message: 'API key not found - check IMDB_KEY env variable.' })
         }
